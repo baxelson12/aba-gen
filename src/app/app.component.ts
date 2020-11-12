462e36ac356feea6e9eb81dc16c16c8f97e499fa
Brad <bradaxelson@gmail.com>
2020-11-12 11:41:25 -0500

    feat: Build out forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   abaForm = this.fb.group({
     sender: this.fb.group({
       bank: [''],
@@ -14,7 +14,29 @@ export class AppComponent {
       user_number: [''],
       description: [''],
     }),
-    recipient: this.fb.array,
+    recipient: this.fb.array([]),
   });
-  constructor(private fb: FormBuilder) {}
+  get recipient(): FormArray {
+    return this.abaForm.get('recipient') as FormArray;
+  }
+  constructor(private fb: FormBuilder) {
+    this.addRecipient();
+  }
+  ngOnInit() {}
+
+  onSubmit() {
+    console.log(this.abaForm.value);
+  }
+
+  addRecipient() {
+    const form = this.fb.group({
+      bsb: [''],
+      transcode: [''],
+      account: [''],
+      amount: [''],
+      reference: [''],
+    });
+
+    this.recipient.push(form);
+  }
 }
