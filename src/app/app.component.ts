b56bd5c4d0b5ee0afa6e8a9c0a61475ec1c0b6cc
Brad <bradaxelson@gmail.com>
2020-11-27 15:26:18 -0500

    feat: Integrate generic error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,70 @@
-import { Component } from '@angular/core';
-import { FormArray, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AppService } from './app.service';
+import { ErrorHandler } from './shared/handlers/error.handler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // Create initial form
   abaForm = this.fb.group({
     sender: this.fb.group({
-      bank: [''],
-      user: [''],
-      userNumber: [''],
-      description: [''],
+      bank: ['', Validators.required],
+      user: ['', [Validators.required]],
+      userNumber: ['', [Validators.required]],
+      description: ['', [Validators.required]],
     }),
     recipient: this.fb.array([]),
   });
+  // Errors
+  errors: any = {};
 
   // Makes it easier to retrieve array controls
   get recipient(): FormArray {
     return this.abaForm.get('recipient') as FormArray;
   }
-  constructor(private fb: FormBuilder, private as: AppService) {
+  // Makes it easier to retrieve sender controls
+  get sender(): FormGroup {
+    return this.abaForm.get('sender') as FormGroup;
+  }
+
+  constructor(
+    private errorHandler: ErrorHandler,
+    private fb: FormBuilder,
+    private as: AppService
+  ) {
     // Start the app with one empty recipient/transaction
     this.addRecipient();
+  }
+
+  ngOnInit() {
+    this.errorHandler.handleErrors(this.abaForm, this.errors);
+    console.log(this.abaForm.value);
   }
 
   // Ship to service, reset form
   onSubmit() {
     this.as.generateAba(this.abaForm.value);
     this.abaForm.reset();
+    this.abaForm.markAsPristine();
+    this.abaForm.markAsUntouched();
   }
 
   // Create one empty recipient/transaction
   addRecipient() {
     const form = this.fb.group({
-      bsb: [''],
-      transactionCode: [''],
-      account: [''],
-      amount: [''],
-      accountTitle: [''],
-      reference: [''],
-      traceBsb: [''],
-      traceAccount: [''],
-      remitter: [''],
+      bsb: ['', [Validators.required]],
+      transactionCode: ['', [Validators.required]],
+      account: ['', [Validators.required]],
+      amount: ['', [Validators.required]],
+      accountTitle: ['', [Validators.required]],
+      reference: ['', [Validators.required]],
+      traceBsb: ['', [Validators.required]],
+      traceAccount: ['', [Validators.required]],
+      remitter: ['', [Validators.required]],
     });
 
     this.recipient.push(form);
